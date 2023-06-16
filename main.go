f9d9840e3ae4e424dcb553734db6c359fe2d43b6
claks <chris.laks@gmail.com>
2023-06-17 00:41:30 +0300

    Centered the timer to terminal using termbox

--- main.go
+++ main.go
@@ -20,16 +20,33 @@ func Timer() time.Duration {
 	}
 	defer termbox.Close()
 
+	fmt.Println("Press the spacebar to start the timer")
 	timerStart := time.Now()
 	timerStopped := true
+
+	termbox.Sync()
+	width, height := termbox.Size()
+	x := (width - 14) / 2
+	y := height / 2
+
+	termbox.SetCursor(x, y)
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	elapsedTime := ""
+	printTimer(elapsedTime, x, y)
+	termbox.HideCursor()
+	termbox.Flush()
+
 	go func() {
 		for {
 			if !timerStopped {
 				timerNow := time.Now()
 				timerElapsed := timerNow.Sub(timerStart)
-				fmt.Printf("\rTIME: %.3f seconds", timerElapsed.Seconds())
+				elapsedTime := fmt.Sprintf("%.3f seconds", timerElapsed.Seconds())
+
+				printTimer(elapsedTime, x, y)
+				termbox.Flush()
 			}
-			time.Sleep(1000)
+			time.Sleep(30 * time.Millisecond)
 		}
 	}()
 
@@ -37,16 +54,18 @@ func Timer() time.Duration {
 		event := termbox.PollEvent()
 		if event.Type == termbox.EventKey && event.Key == termbox.KeySpace {
 			timerStopped = !timerStopped
-
 			if timerStopped {
 				break
 			}
-
-			fmt.Println("\nTimer Started")
 			timerStart = time.Now()
 		}
 	}
 	timerStop := time.Now()
 	return timerStop.Sub(timerStart)
+}
 
+func printTimer(str string, x, y int) {
+	for i, ch := range str {
+		termbox.SetCell(x+i, y, ch, termbox.ColorDefault, termbox.ColorDefault)
+	}
 }
