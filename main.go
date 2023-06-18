2bf059834a3fa81bb00baf537131910299407ee0
claks <chris.laks@gmail.com>
2023-06-19 01:00:18 +0300

    Added simplistic UI

--- main.go
+++ main.go
@@ -4,13 +4,47 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/nsf/termbox-go"
 )
 
 func main() {
-	fmt.Println("Press spacebar to start/stop the timer")
-	duration := Timer()
-	fmt.Printf("Timer stopped, Elapsed time: %.3f seconds\n", duration.Seconds())
+	clearScreen()
+	ui()
+}
+
+func ui() {
+	Uinput := ""
+	prompt := &survey.Select{
+		Message:       "Welcome to GoTSP",
+		Options:       []string{"Speedcube", "Scramble", "View leaderboard", "Timer", "Quit"},
+		FilterMessage: "s",
+		VimMode:       true,
+		Filter:        nil,
+		Description:   nil,
+	}
+
+	survey.AskOne(prompt, &Uinput, nil, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.Question.Text = "!"
+		icons.Question.Format = "red+b"
+	}))
+
+	if Uinput == "Speedcube" {
+		fmt.Println("Speedcube time")
+	} else if Uinput == "Scramble" {
+		fmt.Println("Get a random scramble between 10-16 moves")
+	} else if Uinput == "View leaderboard" {
+		fmt.Println("Nothing much to see here")
+	} else if Uinput == "Timer" {
+		clearScreen()
+		fmt.Println("Start/stop the timer with spacebar")
+		fmt.Println("Press any key to continue to the timer..")
+		fmt.Scanln()
+		clearScreen()
+		duration := Timer()
+		fmt.Printf("Timer stopped, Elapsed time: %.3f seconds\n", duration.Seconds())
+		// return to menu or quit prompt
+	}
 }
 
 func Timer() time.Duration {
@@ -41,7 +75,7 @@ func Timer() time.Duration {
 			if !timerStopped {
 				timerNow := time.Now()
 				timerElapsed := timerNow.Sub(timerStart)
-				elapsedTime := fmt.Sprintf("%.3f seconds", timerElapsed.Seconds())
+				elapsedTime := fmt.Sprintf("%.3f sec", timerElapsed.Seconds())
 
 				printTimer(elapsedTime, x, y)
 				termbox.Flush()
@@ -68,4 +102,8 @@ func printTimer(str string, x, y int) {
 	for i, ch := range str {
 		termbox.SetCell(x+i, y, ch, termbox.ColorDefault, termbox.ColorDefault)
 	}
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
 }
